7e1f3e65e1666e327a71b411db167933da4de040
Aluno - Lab <Aluno - Lab@MMD-LAB02-62>
2026-03-24 20:52:36 -0300

    Adicionar arquivos de projeto.

---
+++ Controllers/AreaPublicaController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProjetoFacul.Controllers
+{
+    public class AreaPublicaController : Controller
+    {
+        public IActionResult PageProdutos()
+        {
+            return View();
+        }
+
+        public IActionResult PageHome()
+        {
+            return View();
+        }
+
+        public IActionResult PageUnidade()
+        {
+            return View();
+        }
+    }
+}
